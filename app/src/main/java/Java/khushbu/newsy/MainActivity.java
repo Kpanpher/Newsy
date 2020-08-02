7e3780519ed58d1be0a70beda5bebc7bc89e62fa
Khushbu <Khushusayhi@gmail.co>
2020-08-02 16:05:03 +0530

    Start

---
+++ app/src/main/java/Java/khushbu/newsy/MainActivity.java
@@ -0,0 +1,31 @@
+package Java.khushbu.newsy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+import Java.khushbu.newsy.ui.login.LoginActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private static int SPLASH_SCREEN_TIME_OUT=2000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this,Second.class);
+
+                startActivity(i);
+            }
+        },SPLASH_SCREEN_TIME_OUT);
+    }
+}
